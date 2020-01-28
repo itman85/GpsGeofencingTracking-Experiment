1ff45edb3748541eec6a7414aca771e1f84acb45
Phan Nguyen <pcnguyen1985@gmail.com>
2020-01-28 16:13:33 +0700

    init project

---
+++ app/src/main/java/phannguyen/sample/gpsgeofencingtrackingexperiment/MainActivity.java
@@ -0,0 +1,34 @@
+package phannguyen.sample.gpsgeofencingtrackingexperiment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(v -> {
+
+        });
+
+        Button stopBtn = findViewById(R.id.stopBtn);
+        stopBtn.setOnClickListener(v -> {
+
+        });
+
+        Button awareBtn = findViewById(R.id.awarenessBtn);
+        awareBtn.setOnClickListener(v -> {
+            //
+            //startActivity(new Intent(MainActivity.this,AwarenessActivity.class));
+            Toast.makeText(MainActivity.this,"This button disable processing",Toast.LENGTH_LONG).show();
+        });
+
+    }
+}
