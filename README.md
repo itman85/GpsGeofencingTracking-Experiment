f0fd9df5b14a584754317c30d3d8d1adbcca5efb
Phan Nguyen <phannguyen@nvg.vn>
2020-02-03 18:08:24 +0700

    add permission location

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GpsGeofencingTracking-Experiment
 Test and experiment gps, geofencing tracking in background
+
+References:
+
+https://developer.android.com/training/location/receive-location-updates#inform-user-background-location-requirement
+
+https://stackoverflow.com/questions/40142331/how-to-request-location-permission-at-runtime
+
+Android code search
+
+1. Permission Controller:
+https://cs.android.com/android/platform/superproject/+/master:packages/apps/PermissionController/src/com/android/packageinstaller/permission/service/RuntimePermissionsUpgradeController.java
+https://cs.android.com/android/platform/superproject/+/master:packages/apps/PermissionController/res/values/strings.xml;l=587
+
--- app/src/main/java/phannguyen/sample/gpsgeofencingtrackingexperiment/MainActivity.java
+++ app/src/main/java/phannguyen/sample/gpsgeofencingtrackingexperiment/MainActivity.java
@@ -1,7 +1,11 @@
 package phannguyen.sample.gpsgeofencingtrackingexperiment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,6 +13,8 @@ import android.widget.Toast;
 import phannguyen.sample.gpsgeofencingtrackingexperiment.helper.WorkManagerHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +35,19 @@ public class MainActivity extends AppCompatActivity {
         awareBtn.setOnClickListener(v -> {
             //
             //startActivity(new Intent(MainActivity.this,AwarenessActivity.class));
-            Toast.makeText(MainActivity.this,"This button disable processing",Toast.LENGTH_LONG).show();
+            //Toast.makeText(MainActivity.this,"This button disable processing",Toast.LENGTH_LONG).show();
+            askLocationPermission();
         });
 
+    }
+
+    private void askLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+        }
     }
 }
